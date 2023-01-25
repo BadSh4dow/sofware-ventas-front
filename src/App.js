1233efd1a1a7f9c86892817957280a7b68ac8b07
BadSh4dow <guidocabrera711@gmail.com>
2023-01-25 20:23:11 -0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TopHeader from './components/header/TopHeader';
+
+function App() {
+  return (
+    <div className='app'>
+      <TopHeader/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/TopHeader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const TopHeader = () => {
+    return(
+        <div className="topHeader">
+            <form className="headerTopForm">
+                <select></select>
+                <input type="date"></input>
+                <select></select>
+            </form>
+            <button type="button"></button>
+        </div>
+    )
+}
+
+export default TopHeader
