7e786468ef6ca85eeae84a80a1d9e0a97769788c
Nicolas <nicolasmarquez005@gmail.com>
2023-02-03 15:18:49 -0300

    lista funcional renglones

--- src/components/body/renglones.jsx
+++ src/components/body/renglones.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { Producto } from "../../producto";
 
 const Renglones = () => {
+    
+    const formRef = useRef()
+
+    const [codigo, setCodigo] = useState()
+    const [t, setT] = useState()
+    const [cantidad, setCantidad] = useState()
+    const [precio, setPrecio] = useState()
+    const [descuentos, setDescuentos] = useState()
+    const [lista, setLista] = useState([])
+
+    function handelButton() {
+        const codigo = formRef.current[0].value
+        const t = formRef.current[3].value
+        const cantidad = formRef.current[4].value
+        const precio = formRef.current[5].value
+        const descuentos = formRef.current[6].value
+
+        const producto = new Producto(codigo, t, cantidad, precio, descuentos)
+
+        setLista(lista.concat(producto))
+
+        setCodigo(formRef.current[0].value)
+        setT(formRef.current[3].value)
+        setCantidad(formRef.current[4].value)
+        setPrecio(formRef.current[5].value)
+        setDescuentos(formRef.current[6].value)
+    }
     return(
         <div className="renglones">
             <div className="cargas">
-                <form className="formCargas">
+                <form ref={formRef} className="formCargas">
                     <label htmlFor="codigo">Codigo</label>
                     <input id="codigo" type="number" />
                     <button type="button">...</button>
@@ -24,12 +55,18 @@ const Renglones = () => {
                     <label htmlFor="descuentos">Descuentos</label>
                     <input id="descuentos" type="number" />
 
-                    <button type="button">+</button>
+                    <button onClick={handelButton} type="button">+</button>
                 </form>            
             </div>
             <div className="listado">
                 <ul>
-                    <li>ASDSad</li>
+                    {
+                        lista.map((producto, key) => {
+                            return(
+                                <li key={key}>{`${producto.codigo} ${producto.t} ${producto.cantidad} ${producto.precio} ${producto.descuentos}`}</li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
         </div>
---
+++ src/producto.js
@@ -0,0 +1,9 @@
+export class Producto{
+    constructor(codigo, t, cantidad, precio, descuentos){
+        this.codigo = codigo
+        this.t = t
+        this.cantidad = cantidad
+        this.precio = precio
+        this.descuentos = descuentos
+    }
+}
